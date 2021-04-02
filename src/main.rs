c6084ecf0879badf3640222141c6d6058056d2d3
Lazthedude <68569218+Lazthedude@users.noreply.github.com>
2021-04-02 16:15:23 -0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use fltk::{app, window::*};
 fn main() {
     let app = app::App::default();
     let mut wind = Window::new(100, 100, 400, 300, "Rustcord");
+    wind.make_resizable(true);
     wind.end();
     wind.show();
     app.run().unwrap();

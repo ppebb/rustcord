539319003b509e6b5b71cdc2d2e40406b277803f
Lazthedude <68569218+Lazthedude@users.noreply.github.com>
2021-04-02 21:07:20 -0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ fn main() {
     let app = app::App::default();
     let mut wind = Window::new(100, 100, 400, 300, "Rustcord");
     wind.make_resizable(true);
+    wind = wind.center.screen();
     wind.end();
     wind.show();
     app.run().unwrap();

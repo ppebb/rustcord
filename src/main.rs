e15e57e6c582c30b7cca813f410b714f15665faa
Lazthedude <68569218+Lazthedude@users.noreply.github.com>
2021-04-04 20:22:26 -0300

    add basic chat and message functionality

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,26 @@
-use fltk::{app, window::*, frame::*, };
+use fltk::{app, window::*, frame::*, browser::*, button::*, input::*};
 
 fn main() {
     let app = app::App::default();
     let mut wind = Window::new(100, 100, 1000, 500, "Rustcord");
-    let mut servers = Frame::default().with_size(50, 500).with_label("servers").set_frame(FrameType::EngravedBox);
-    let mut top_bar = Frame::default().with_pos(50, 0).with_size(950, 50).with_label("top bar").set_frame(FrameType::EngravedBox);
-    let mut channels = Frame::default().with_pos(50, 50).with_size(200, 400).with_label("channels").set_frame(FrameType::EngravedBox);
-    let mut chat = Frame::default().with_pos(250, 50).with_size(500, 400).with_label("chat").set_frame(FrameType::EngravedBox);
-    let mut info = Frame::default().with_pos(50, 450).with_size(200, 50).with_label("info").set_frame(FrameType::EngravedBox);
-    let mut chat_box = Frame::default().with_pos(250, 450).with_size(500, 50).with_label("chat box").set_frame(FrameType::EngravedBox);
-    let mut members = Frame::default().with_pos(750, 50).with_size(250, 450).with_label("members").set_frame(FrameType::EngravedBox);
+    let servers = Frame::default().with_size(50, 500).with_label("servers").set_frame(FrameType::EngravedBox);
+    let top_bar = Frame::default().with_pos(50, 0).with_size(950, 50).with_label("top bar").set_frame(FrameType::EngravedBox);
+    let channels = Frame::default().with_pos(50, 50).with_size(200, 400).with_label("channels").set_frame(FrameType::EngravedBox);
+    let info = Frame::default().with_pos(50, 450).with_size(200, 50).with_label("info").set_frame(FrameType::EngravedBox);
+    let members = Frame::default().with_pos(750, 50).with_size(250, 450).with_label("members").set_frame(FrameType::EngravedBox);
+    let mut chat_messages = HoldBrowser::default().with_size(500, 400).with_pos(250, 50);
+    //let mut channels_list = HoldBrowser::default().with_size(200, 400).with_pos(50, 50).add("channel");
+    let mut but1 = ReturnButton::default().with_pos(700, 450).with_size(50, 50).with_label("send");
+    let mut chat_input = Input::default().with_pos(250, 450).with_size(450, 50);
+    //app::background(54, 57, 63);
     wind.make_resizable(true);
     wind.end();
     wind.show();
+    
+    but1.set_callback(move || {
+        chat_messages.add(&chat_input.value());
+        chat_input.set_value("");
+    });
     app.run().unwrap();
+
 }

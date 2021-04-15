dbea31549b2a2eaa61d3886184044be67760f01e
Chik3r <ikervaraalarcon@gmail.com>
2021-04-15 19:47:58 -0400

    Move the ui creation into a struct in a new module

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use fltk::{app, window::*, frame::*, browser::*, button::*, input::*};
+use fltk::prelude::BrowserExt;
 use futures_channel;
 use tokio::{self, sync::mpsc};
 use networking::{connect_to_discord, data::{gateway::GatewayPayload, receive::handle_messages, sendable}};
@@ -13,6 +13,7 @@ extern crate pretty_env_logger;
 extern crate log;
 
 mod networking;
+mod ui;
 
 #[tokio::main]
 async fn main() {
@@ -27,27 +28,11 @@ async fn main() {
         .default_headers(headers)
         .build().unwrap();
 
-    let app = app::App::default();
-    let mut wind = Window::new(100, 100, 1000, 500, "Rustcord");
-    let servers = Frame::default().with_size(50, 500).with_label("servers").set_frame(FrameType::EngravedBox);
-    let top_bar = Frame::default().with_pos(50, 0).with_size(950, 50).with_label("top bar").set_frame(FrameType::EngravedBox);
-    let channels = Frame::default().with_pos(50, 50).with_size(200, 400).with_label("channels").set_frame(FrameType::EngravedBox);
-    let info = Frame::default().with_pos(50, 450).with_size(200, 50).with_label("info").set_frame(FrameType::EngravedBox);
-    let members = Frame::default().with_pos(750, 50).with_size(250, 450).with_label("members").set_frame(FrameType::EngravedBox);
-    let mut chat_messages = HoldBrowser::default().with_size(500, 400).with_pos(250, 50);
-    let mut but1 = ReturnButton::default().with_pos(700, 450).with_size(50, 50).with_label("send");
-    let mut chat_input = Input::default().with_pos(250, 450).with_size(450, 50);
-    wind.make_resizable(true);
-    wind.end();
-    wind.show();
+    let ui = ui::RustcordUI::new();
 
-    but1.set_callback(move || {
-        // Only add the value if the input isn't empty
-        if !chat_input.value().is_empty() {
-            chat_messages.add(&chat_input.value());
-            chat_input.set_value("");
-        }
-    });
+    // Add a test item to the ui
+    let mut b = ui.clone();
+    b.chat_messages.add("item");
     
     // Make a mpsc for sending messages to discord and another for receiving messages from discord
     let (send_tx, send_rx) = futures_channel::mpsc::unbounded::<Message>();
@@ -57,7 +42,7 @@ async fn main() {
     tokio::spawn(handle_messages(client.clone(), receive_rx, send_tx.clone())); // Spawn a thread to handle the messages received from the websocket
     tokio::spawn(sendable::send_identify(token.clone(), send_tx.clone())); // Spawn a thread to send an identify message to the websocket
     
-    app.run().unwrap();
+    ui.app.run().unwrap();
 }
 
 async fn connect(write_rx: futures_channel::mpsc::UnboundedReceiver<Message>, read_tx: mpsc::Sender<GatewayPayload>) {
---
+++ src/ui.rs
@@ -0,0 +1,61 @@
+use fltk::*;
+
+#[derive(Clone)]
+pub struct RustcordUI {
+    pub app: app::App,
+    pub window: window::DoubleWindow,
+    pub guilds: frame::Frame,
+    pub top_bar: frame::Frame,
+    pub channels: frame::Frame,
+    pub info: frame::Frame,
+    pub members: frame::Frame,
+    pub chat_messages: browser::HoldBrowser,
+    pub chat_send_button: button::ReturnButton,
+    pub chat_text_input: input::Input
+}
+
+impl Default for RustcordUI {
+    fn default() -> Self {
+        let mut ui = RustcordUI {
+            app: app::App::default(),
+            window: window::Window::new(100, 100, 1000, 500, "Rustcord"),
+            guilds: frame::Frame::default().with_size(50, 500).with_label("servers"),
+            top_bar: frame::Frame::default().with_pos(50, 0).with_size(950, 50).with_label("top bar"),
+            channels: frame::Frame::default().with_pos(50, 50).with_size(200, 400).with_label("channels"),
+            info: frame::Frame::default().with_pos(50, 450).with_size(200, 50).with_label("info"),
+            members: frame::Frame::default().with_pos(750, 50).with_size(250, 450).with_label("members"),
+            chat_messages: browser::HoldBrowser::default().with_size(500, 400).with_pos(250, 50),
+            chat_send_button: button::ReturnButton::default().with_pos(700, 450).with_size(50, 50).with_label("send"),
+            chat_text_input: input::Input::default().with_pos(250, 450).with_size(450, 50)
+        };
+
+        // Set the framing of the different widgets
+        ui.guilds.set_frame(FrameType::EngravedBox);
+        ui.top_bar.set_frame(FrameType::EngravedBox);
+        ui.channels.set_frame(FrameType::EngravedBox);
+        ui.info.set_frame(FrameType::EngravedBox);
+        ui.members.set_frame(FrameType::EngravedBox);
+
+        ui.window.make_resizable(true);
+        ui.window.end();
+        ui.window.show();
+
+        // Create a callback for when the button is pressed or enter is pressed
+        let mut ui_c = ui.clone();
+        ui.chat_send_button.set_callback(move || {
+            // Only add the value if the input isn't empty
+            if !ui_c.chat_text_input.value().is_empty() {
+                ui_c.chat_messages.add(&ui_c.chat_text_input.value());
+                ui_c.chat_text_input.set_value("");
+            }
+        });
+
+        ui
+    }
+}
+
+impl RustcordUI {
+    pub fn new() -> Self {
+        Default::default()
+    }
+}
